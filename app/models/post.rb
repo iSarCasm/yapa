f0c1c9be12fe34abf19e92f2b1881a0719f3b906
Igor Tsykalo <sarcasm008@gmail.com>
2022-04-26 15:40:16 -0600

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: posts
-#
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  user_id    :bigint           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 class Post < ApplicationRecord
   belongs_to :user
 end
