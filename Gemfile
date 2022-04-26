2f86ca5a16c90e8bc6ee41a28ce3721acee474b2
Igor Tsykalo <sarcasm008@gmail.com>
2022-04-26 15:29:43 -0600

    Require User name during sign up

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'annotate' # generate db annotations for models, factories, etc.
+  gem 'pry', '~> 0.13.1' # debugger
 end
 
 group :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string(30)
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 30, minimum: 4 }
 end
