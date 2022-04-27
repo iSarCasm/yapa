7f13197e3dd859e8f1451606695dc8538beab121
Igor Tsykalo <sarcasm008@gmail.com>
2022-04-27 13:56:22 -0600

    Make Sign In a modal

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -4,11 +4,13 @@ describe 'Users', type: :feature do
   it 'can sign up' do
     visit '/'
     click_button 'Sign Up'
-    fill_in 'Email', with: 'john@doe.com'
-    fill_in 'Name', with: 'John Doe'
-    fill_in 'Password', with: '123123'
-    fill_in 'Password confirmation', with: '123123'
-    click_button 'Submit'
+    within '.container' do
+      fill_in 'Email', with: 'john@doe.com'
+      fill_in 'Name', with: 'John Doe'
+      fill_in 'Password', with: '123123'
+      fill_in 'Password confirmation', with: '123123'
+      click_button 'Submit'
+    end
 
     expect(page).to have_current_path '/'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
@@ -23,7 +25,7 @@ describe 'Users', type: :feature do
     click_button 'Sign In'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_button 'Submit'
+    click_button 'Log in'
 
     expect(page).to have_current_path '/'
     expect(page).to have_content 'Signed in successfully.'
@@ -40,7 +42,7 @@ describe 'Users', type: :feature do
 
     expect(page).to have_current_path '/'
     expect(page).to have_content 'Signed out successfully.'
-    expect(page).to have_button 'Sign In'
+    expect(page).to have_button 'Log in'
   end
 
   it 'can edit their profile' do
@@ -49,12 +51,14 @@ describe 'Users', type: :feature do
 
     visit '/'
     click_link user.name
-    fill_in 'Name', with: 'John Doe'
-    fill_in 'Email', with: 'newmail@mail.com'
-    fill_in 'Password', with: '123123'
-    fill_in 'Password confirmation', with: '123123'
-    fill_in 'Current password', with: user.password
-    click_button 'Update'
+    within '.container' do
+      fill_in 'Name', with: 'John Doe'
+      fill_in 'Email', with: 'newmail@mail.com'
+      fill_in 'Password', with: '123123'
+      fill_in 'Password confirmation', with: '123123'
+      fill_in 'Current password', with: user.password
+      click_button 'Update'
+    end
 
     expect(page).to have_current_path '/'
     expect(page).to have_content 'Your account has been updated successfully.'
